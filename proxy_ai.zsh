62148bc1085982fd56f0eabed706d629e5c58143
CuongND <5164308+duccuong2808@users.noreply.github.com>
2026-01-22 09:36:44 +0700

    fix: Sửa lỗi chính tả biến `ANTIGRATIVY_AUTH_TOKEN`, thêm biến mẫu tương ứng và điều kiện hóa việc đặt các mô hình Anthropic mặc định.

--- proxy_ai.zsh
+++ proxy_ai.zsh
@@ -7,7 +7,7 @@ CLIProxy="true"
 # proxypal = https://github.com/heyhuynhgiabuu/proxypal
 # antigravity-tools = https://github.com/lbjlaq/Antigravity-Manager/blob/main/README_EN.md
 : ${PROXY_PROVIDER:="proxypal"}
-: ${ANTIGRATIVY_AUTH_TOKEN:="[Please define at constant_secret.zsh file]"}
+: ${ANTIGRAVITY_AUTH_TOKEN:="[Please define at constant_secret.zsh file]"}
 
 if [ "$CLIProxy" = "true" ]; then
   if [ "$PROXY_PROVIDER" = "ccs" ]; then
@@ -22,12 +22,12 @@ if [ "$CLIProxy" = "true" ]; then
 
   if [ "$PROXY_PROVIDER" = "antigravity-tools" ]; then
     export ANTHROPIC_BASE_URL="http://127.0.0.1:8045"
-    export ANTHROPIC_AUTH_TOKEN="$ANTIGRATIVY_AUTH_TOKEN"
+    export ANTHROPIC_AUTH_TOKEN="$ANTIGRAVITY_AUTH_TOKEN"
+  else
+    # For Claude Code 2.x
+    export ANTHROPIC_DEFAULT_OPUS_MODEL="gemini-claude-opus-4-5-thinking"
+    # export ANTHROPIC_DEFAULT_SONNET_MODEL="gemini-claude-sonnet-4-5"
+    export ANTHROPIC_DEFAULT_SONNET_MODEL="gemini-claude-sonnet-4-5-thinking"
+    export ANTHROPIC_DEFAULT_HAIKU_MODEL="gemini-3-flash-preview"
   fi
-
-  # For Claude Code 2.x
-  export ANTHROPIC_DEFAULT_OPUS_MODEL="gemini-claude-opus-4-5-thinking"
-  # export ANTHROPIC_DEFAULT_SONNET_MODEL="gemini-claude-sonnet-4-5"
-  export ANTHROPIC_DEFAULT_SONNET_MODEL="gemini-claude-sonnet-4-5-thinking"
-  export ANTHROPIC_DEFAULT_HAIKU_MODEL="gemini-3-flash-preview"
 fi
